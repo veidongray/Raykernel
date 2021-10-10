b36eb7c5ff838181e4787231a155d90a1456cb4c
yivies <36105318+YIVIES@users.noreply.github.com>
2021-10-10 23:03:24 +0800

    Add files via upload

--- kernel.c
+++ kernel.c
@@ -8,45 +8,53 @@ inline static void hlt(void);
 inline static void sti(void);
 inline static void cli(void);
 int putck(char, unsigned int);
+int putsk(char *, unsigned int);
 
 int main(void)
 {
-    unsigned int addr = 0xb8000;
-    for (int i = 0; i < 128; i++) {
-        putck(i, addr);
-        addr += 0x2;
-    }
-    while (1) {
-        hlt();
-    }
-    return 0;
+	char str[7] = "RayOS!";
+	putsk(str, 6);
+	while (1) {
+		hlt();
+	}
+	return 0;
 }
 
 inline static void hlt(void)
 {
-    asmk("hlt");
+	asmk("hlt");
 }
 
 inline static void sti(void)
 {
-    asmk("sti");
+	asmk("sti");
 }
 
 inline static void cli(void)
 {
-    asmk("cli");
+	asmk("cli");
 }
 
 int putck(char ch, unsigned int addr)
 {
-    asmk("push %eax");
-    asmk("push %ebx");
-    asmk("mov %0, %%al"::"g"(ch));
-    asmk("mov %0, %%ebx"::"g"(addr));
-    asmk("mov %al, (%ebx)");
-    asmk("pop %ebx");
-    asmk("pop %eax");
-    return 0;
+	asmk("push %eax");
+	asmk("push %ebx");
+	asmk("mov %0, %%al"::"g"(ch));
+	asmk("mov %0, %%ebx"::"g"(addr));
+	asmk("mov %al, (%ebx)");
+	asmk("pop %ebx");
+	asmk("pop %eax");
+	return 0;
+}
+
+int putsk(char * _str, unsigned int _size)
+{
+	unsigned int addr = 0xb8000;
+	for (int i = 0; i < _size; i++) {
+		putck(_str[i], addr);
+		addr += 0x2;
+	}
+	return 0;
 }
 #ifdef __cplusplus
 }
